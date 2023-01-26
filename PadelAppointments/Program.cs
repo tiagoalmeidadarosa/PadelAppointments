6b06b29a76975f4cd583bf03c38e23f5024b0c09
Tiago Almeida da Rosa <tiago.almeida@poatek.com>
2023-01-26 11:48:04 -0300

    Put and Delete endpoints

--- PadelAppointments/Program.cs
+++ PadelAppointments/Program.cs
@@ -2,6 +2,8 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 using PadelAppointments;
 using PadelAppointments.Entities;
+using PadelAppointments.Models.Requests;
+using PadelAppointments.Models.Responses;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -32,27 +34,89 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.MapGet("/courts", async (ApplicationDbContext db) => await db.Courts.ToListAsync());
-
-app.MapGet("/courts/{id}", async (ApplicationDbContext db, int id) => await db.Courts.FindAsync(id));
-
-app.MapGet("/courts/{id}/appointments", async (ApplicationDbContext db, int id) 
-    => await db.Appointments
-        .Where(a => a.CourtId == id)
-        .ToListAsync());
-
-app.MapPost("/courts/{id}/appointments", async (ApplicationDbContext db, int id, Appointment appointment) =>
+app.MapGet("/courts", async (ApplicationDbContext db) =>
 {
-    appointment.CourtId = id;
+    return await db.Courts
+        .Select(c => new CourtsResponse()
+        {
+            Id = c.Id,
+            Name = c.Name,
+        })
+        .ToListAsync();
+})
+.Produces<List<CourtsResponse>>();
+
+app.MapGet("/courts/{id}/appointments", async (ApplicationDbContext db, int id, DateOnly date) =>
+{
+    return await db.Appointments
+        .Where(a => a.CourtId == id && a.Date == date)
+        .Select(c => new AppointmentResponse()
+        {
+            Id = c.Id,
+            Date = c.Date,
+            Time = c.Time,
+            CustomerName = c.CustomerName,
+            CustomerPhoneNumber = c.CustomerPhoneNumber,
+        })
+        .ToListAsync();
+})
+.Produces<List<AppointmentResponse>>();
+
+app.MapPost("/courts/{id}/appointments", async (ApplicationDbContext db, int id, AppointmentRequest request) =>
+{
+    var appointment = new Appointment()
+    {
+        Date = DateOnly.FromDateTime(request.Date),
+        Time = TimeOnly.FromDateTime(request.Date),
+        CustomerName = request.CustomerName,
+        CustomerPhoneNumber = request.CustomerPhoneNumber,
+        CourtId = id,
+    };
+
     await db.Appointments.AddAsync(appointment);
     await db.SaveChangesAsync();
 
     return Results.Created($"/courts/{id}/appointments/{appointment.Id}", appointment);
-});
+})
+.Produces(StatusCodes.Status201Created);
 
-app.MapGet("/courts/{id}/appointments/{appointmentId}", async (ApplicationDbContext db, int id, int appointmentId) 
-    => await db.Appointments
-        .Where(a => a.CourtId == id)
-        .FirstOrDefaultAsync(a => a.Id == appointmentId));
+app.MapPut("/courts/{id}/appointments/{appointmentId}", async (ApplicationDbContext db, int id, 
+    int appointmentId, UpdateAppointmentRequest request) =>
+{
+    var appointment = await db.Appointments
+        .FirstOrDefaultAsync(a => a.Id == appointmentId && a.CourtId == id);
+
+    if (appointment == null)
+    {
+        return Results.NotFound();
+    }
+
+    appointment.CustomerName = request.CustomerName;
+    appointment.CustomerPhoneNumber = request.CustomerPhoneNumber;
+
+    await db.SaveChangesAsync();
+
+    return Results.NoContent();
+})
+.Produces(StatusCodes.Status204NoContent)
+.Produces(StatusCodes.Status404NotFound);
+
+app.MapDelete("/courts/{id}/appointments/{appointmentId}", async (ApplicationDbContext db, int id, int appointmentId) =>
+{
+    var appointment = await db.Appointments
+        .FirstOrDefaultAsync(a => a.Id == appointmentId && a.CourtId == id);
+
+    if (appointment == null)
+    {
+        return Results.NotFound();
+    }
+
+    db.Appointments.Remove(appointment);
+    await db.SaveChangesAsync();
+
+    return Results.Ok();
+})
+.Produces(StatusCodes.Status200OK)
+.Produces(StatusCodes.Status404NotFound);
 
 app.Run();
