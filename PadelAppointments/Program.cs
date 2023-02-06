602333d705959e372361e82a2457541a97033aeb
Tiago Almeida da Rosa <tiago.almeida@poatek.com>
2023-02-06 14:20:22 -0300

    Swagger ui when the app is deployed

--- PadelAppointments/Program.cs
+++ PadelAppointments/Program.cs
@@ -36,14 +36,14 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI(c =>
     {
         c.SwaggerEndpoint("/swagger/v1/swagger.json", "PadelAppointments API V1");
     });
-}
+//}
 
 app.UseHttpsRedirection();
 app.UseCors();
